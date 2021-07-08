74674e02933d808e738097cb2e5264b885cad34f
Shai Aharoni <saharoni@gmail.com>
2021-07-08 20:42:09 +0300

    Day 15 - Part 1

--- PuzzleSolvers/Day15PuzzleSolver.cs
+++ PuzzleSolvers/Day15PuzzleSolver.cs
@@ -13,7 +13,40 @@ namespace AOC2015.PuzzleSolvers
 
             List<Recepie> allPossibleRecepies = GetAllPossibleRecepies(ingredients);
 
-            return string.Empty;
+            int highestRecepieScore = 0;
+            foreach (Recepie recepie in allPossibleRecepies)
+            {
+                int recepieScore = CalculateRecepieScore(recepie);
+                if (recepieScore > highestRecepieScore)
+                {
+                    highestRecepieScore = recepieScore;
+                }
+            }
+
+            return highestRecepieScore.ToString();
+        }
+
+        private int CalculateRecepieScore(Recepie recepie)
+        {
+            int totalCapacity = 0;
+            int totalDurability = 0;
+            int totalFlavor = 0;
+            int totalTexture = 0;
+
+            foreach (KeyValuePair<Ingredient, int> ingredientDetails in recepie.Contents)
+            {
+                totalCapacity += ingredientDetails.Key.Capacity * ingredientDetails.Value;
+                totalDurability += ingredientDetails.Key.Durability * ingredientDetails.Value;
+                totalFlavor += ingredientDetails.Key.Flavor * ingredientDetails.Value;
+                totalTexture += ingredientDetails.Key.Texture * ingredientDetails.Value;
+            }
+
+            if (totalCapacity <= 0 || totalDurability <= 0 || totalFlavor <= 0 || totalTexture <= 0)
+            {
+                return 0;
+            }
+
+            return totalCapacity * totalDurability * totalFlavor * totalTexture;
         }
 
         private List<Recepie> GetAllPossibleRecepies(List<Ingredient> ingredients)
@@ -22,36 +55,34 @@ namespace AOC2015.PuzzleSolvers
 
             for (int numOfSpoons1 = 0; numOfSpoons1 <= 100; numOfSpoons1++)
             {
-                var recepie = new Recepie();
-                if (numOfSpoons1 != 0)
-                {
-                    recepie.Contents.Add(ingredients[0], numOfSpoons1);
-                }
-
                 for (int numOfSpoons2 = 0; numOfSpoons2 <= (100 - numOfSpoons1); numOfSpoons2++)
                 {
-                    if (numOfSpoons2 != 0)
-                    {
-                        recepie.Contents.Add(ingredients[1], numOfSpoons2);
-                    }
-                    
                     for (int numOfSpoons3 = 0; numOfSpoons3 <= (100 - numOfSpoons1 - numOfSpoons2); numOfSpoons3++)
                     {
+                        var recepie = new Recepie();
+                        if (numOfSpoons1 != 0)
+                        {
+                            recepie.Contents.Add(ingredients[0], numOfSpoons1);
+                        }
+
+                        if (numOfSpoons2 != 0)
+                        {
+                            recepie.Contents.Add(ingredients[1], numOfSpoons2);
+                        }
 
                         if (numOfSpoons3 != 0)
                         {
                             recepie.Contents.Add(ingredients[2], numOfSpoons3);
                         }
 
-                        for (int numOfSpoons4 = 0; numOfSpoons4 < (100 - numOfSpoons1 - numOfSpoons2 - numOfSpoons3); numOfSpoons4++)
-                        {
+                        int numOfSpoons4 = 100 - numOfSpoons3 - numOfSpoons2 - numOfSpoons1;
 
-                            if (numOfSpoons4 != 0)
-                            {
-                                recepie.Contents.Add(ingredients[3], numOfSpoons4);
-                                break;
-                            }
+                        if (numOfSpoons4 != 0)
+                        {
+                            recepie.Contents.Add(ingredients[3], numOfSpoons4);
                         }
+                      
+                        allPossibleRecepies.Add(recepie);
                     }
                 }
             }
