0ef1c344f65bf76320f1bb1b884fe915e6dd932a
Shai Aharoni <saharoni@gmail.com>
2021-07-09 21:10:07 +0300

    Day 16 - Part 2

--- PuzzleSolvers/Day16PuzzleSolver.cs
+++ PuzzleSolvers/Day16PuzzleSolver.cs
@@ -11,6 +11,14 @@ namespace AOC2015.PuzzleSolvers
     {
         public string SolvePuzzlePart1()
         {
+            List<AuntSue> allAunts = GetInputAunts();
+
+            int auntId = FindAuntMatch(allAunts);
+            return auntId.ToString();
+        }
+
+        private static List<AuntSue> GetInputAunts()
+        {
             string[] inputLines = InputFilesHelper.GetInputFileLines("day16.txt");
 
             var allAunts = new List<AuntSue>();
@@ -19,19 +27,16 @@ namespace AOC2015.PuzzleSolvers
 
             foreach (string inputLine in inputLines)
             {
-               AuntSue newAunt = builder.Build(inputLine);
-               allAunts.Add(newAunt);
+                AuntSue newAunt = builder.Build(inputLine);
+                allAunts.Add(newAunt);
             }
 
-            int auntId = FindAuntMatch(allAunts);
-            return auntId.ToString();
+            return allAunts;
         }
 
         private int FindAuntMatch(List<AuntSue> allAunts)
         {
-
             List<AuntSue> matchedAunts = new List<AuntSue>(allAunts);
-
             Dictionary<string, int> propertiesToMatch = new Dictionary<string, int>
             {
                 {"children", 3 },
@@ -55,9 +60,52 @@ namespace AOC2015.PuzzleSolvers
             return matchedAunts[0].Id;
         }
 
+        private int FindAuntMatchWithRanges(List<AuntSue> allAunts)
+        {
+            List<AuntSue> matchedAunts = new List<AuntSue>(allAunts);
+            Dictionary<string, int> propertiesToMatch = new Dictionary<string, int>
+            {
+                {"children", 3 },
+                {"cats", 7 },
+                {"samoyeds", 2},
+                {"pomeranians", 3},
+                {"vizslas", 0 },
+                {"akitas", 0 },
+                {"goldfish", 5 },
+                {"trees", 3 },
+                {"cars", 2 },
+                {"perfumes", 1 }
+            };
+
+            foreach (KeyValuePair<string, int> property in propertiesToMatch)
+            {
+                if (property.Key == "cats" || property.Key == "trees")
+                {
+                    matchedAunts = matchedAunts.FindAll(aunt =>
+                    !aunt.Properties.ContainsKey(property.Key) || aunt.Properties[property.Key] > property.Value);
+
+                } else if (property.Key == "pomeranians" || property.Key == "goldfish")
+                {
+                    matchedAunts = matchedAunts.FindAll(aunt =>
+                 !aunt.Properties.ContainsKey(property.Key) || aunt.Properties[property.Key] < property.Value);
+
+                } else
+                {
+                    matchedAunts = matchedAunts.FindAll(aunt =>
+                     !aunt.Properties.ContainsKey(property.Key) || aunt.Properties[property.Key] == property.Value);
+                }
+            }
+
+            return matchedAunts[0].Id;
+        }
+
+
         public string SolvePuzzlePart2()
         {
-            throw new NotImplementedException();
+            List<AuntSue> allAunts = GetInputAunts();
+
+            int auntId = FindAuntMatchWithRanges(allAunts);
+            return auntId.ToString();
         }
     }
 }
