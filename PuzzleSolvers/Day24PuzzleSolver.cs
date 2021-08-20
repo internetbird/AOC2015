8a0de7746bbbc862b59e36117b23826d84b4b3db
Shai Aharoni <saharoni@gmail.com>
2021-08-20 19:01:59 +0300

    Day 24 Part 2!

--- PuzzleSolvers/Day24PuzzleSolver.cs
+++ PuzzleSolvers/Day24PuzzleSolver.cs
@@ -85,7 +85,74 @@ namespace AOC2015.PuzzleSolvers
 
         public string SolvePuzzlePart2()
         {
-            throw new NotImplementedException();
+            string[] inputLines = InputFilesHelper.GetInputFileLines("day24.txt");
+
+            List<ulong> weights = inputLines.Select(line => ulong.Parse(line)).ToList();
+
+            int solutionsCounter = 0;
+            var random = new Random();
+            ulong weightForEachGroup = weights.Sum() / 4;
+
+            while (solutionsCounter < 1000)
+            {
+                var groups = new List<List<ulong>>
+                {
+                     new List<ulong>(),
+                     new List<ulong>(),
+                     new List<ulong>(),
+                     new List<ulong>()
+                };
+
+                foreach (ulong weight in weights)
+                {
+                    int randomGroupIndex = random.Next(4);
+
+                    List<ulong> choosenGroup = groups[randomGroupIndex];
+
+                    if (choosenGroup.Sum() + weight <= weightForEachGroup)
+                    {
+                        choosenGroup.Add(weight);
+
+                    }
+                    else if (groups[(randomGroupIndex + 1) % 4].Sum() + weight <= weightForEachGroup)
+                    {
+                        groups[(randomGroupIndex + 1) % 4].Add(weight);
+                    }
+                    else if (groups[(randomGroupIndex + 2) % 4].Sum() + weight <= weightForEachGroup)
+                    {
+                        groups[(randomGroupIndex + 2) % 4].Add(weight);
+                    }
+                    else
+                    {
+                        groups[(randomGroupIndex + 3) % 4].Add(weight);
+                    }
+                }
+
+                if (groups[0].Sum() == groups[1].Sum() 
+                    && groups[1].Sum() == groups[2].Sum() 
+                    && groups[2].Sum() == groups[3].Sum())
+                {
+                    solutionsCounter++;
+
+                    List<ulong> minSizeGroup = GetMinSizeGroup(groups);
+
+                    if (minSizeGroup.Count < _minNumOfItems)
+                    {
+                        _minNumOfItems = minSizeGroup.Count;
+                        _minMultiplier = minSizeGroup.Multiply();
+
+                        Console.WriteLine($"Found new min size group with size : {_minNumOfItems} and QE : {minSizeGroup.Multiply()}");
+
+                    }
+                    else if (minSizeGroup.Count == _minNumOfItems && minSizeGroup.Multiply() < _minMultiplier)
+                    {
+                        _minMultiplier = minSizeGroup.Multiply();
+                        Console.WriteLine($"Found a new QE min : { minSizeGroup.Multiply()} for group size : {minSizeGroup.Count}");
+                    }
+                }
+            }
+
+            return _minMultiplier.ToString();
         }
     }
 }
